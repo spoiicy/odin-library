e29f5194972c84b6f008ae1e7af113e74c62569b
spoiicy <akshitmaheshwary@gmail.com>
2023-02-19 19:19:32 +0530

    books can be added using form now

--- script.js
+++ script.js
@@ -16,34 +16,62 @@ function book(title,author,pages,read_status){
     this.read_status = read_status;
 }
 
+function addBook(card_container,new_book){
+
+    const card_element = document.createElement('div');    
+        card_element.classList.add('card');
+        
+        const book_title = document.createElement('h3');
+        book_title.textContent = new_book.title;
+        card_element.appendChild(book_title);
+        
+        const book_author = document.createElement('p');
+        book_author.textContent = new_book.author;
+        card_element.appendChild(book_author);
+        
+        const book_pages = document.createElement('p');
+        book_pages.textContent = new_book.pages;
+        card_element.appendChild(book_pages);
+        
+        const book_status = document.createElement('p');
+        book_status.textContent = new_book.read_status;
+        card_element.appendChild(book_status);
+
+        card_container.appendChild(card_element);
+}
+
 
 function addBooksToLibrary(myLibrary){
    
     const card_container = document.querySelector('.card-container');   
     
     myLibrary.forEach((book) => {
-        const card_element = document.createElement('div');    
-        card_element.classList.add('card');
-        
-        const book_title = document.createElement('h3');
-        book_title.textContent = book.title;
-        card_element.appendChild(book_title);
-        
-        const book_author = document.createElement('p');
-        book_author.textContent = book.author;
-        card_element.appendChild(book_author);
-        
-        const book_pages = document.createElement('p');
-        book_pages.textContent = book.pages;
-        card_element.appendChild(book_pages);
-        
-        const book_status = document.createElement('p');
-        book_status.textContent = book.read_status;
-        card_element.appendChild(book_status);
-
-        card_container.appendChild(card_element);
+        addBook(card_container,book);
     });
 }
+
+
+function collectAndAddInfo(){
+    const card_container = document.querySelector('.card-container');
+
+    const form_title = document.querySelector('#title').value;
+    const form_author = document.querySelector('#author').value;
+    const form_pages = document.querySelector('#pages').value;
+    const form_read_status = document.querySelector('#read_status').value;
+    
+    const new_book = new book(form_title,form_author,form_pages,form_read_status);
+    myLibrary.push(new_book); 
+    
+    addBook(card_container,new_book);
+
+}
+
+
+
+const form_button = document.querySelector('.modal button');
+form_button.addEventListener('click',()=>{
+    collectAndAddInfo();
+});
 
 
 
@@ -53,15 +81,13 @@ const book2 = new book('Godaan','Premchand','50','Read');
 const book3 = new book('Godaan','Premchand','50','Read');
 const book4 = new book('Godaan','Premchand','50','Read');
 const book5 = new book('Godaan','Premchand','50','Read');
-const book6 = new book('Godaan','Premchand','50','Read');
-const book7 = new book('Godaan','Premchand','50','Read');
+
 
 myLibrary.push(book1);
 myLibrary.push(book2);
 myLibrary.push(book3);
 myLibrary.push(book4);
 myLibrary.push(book5);
-myLibrary.push(book6);
-myLibrary.push(book7);
+
 addBooksToLibrary(myLibrary);
 console.log(myLibrary);
